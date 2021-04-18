0a5b923b0a8490e2e5dd1fcb62ef0056eea4d6ab
Kellie Manasco <kellie.manasco@gmail.com>
2021-04-17 21:58:49 -0400

    Webpack setup, class constructor added in index.js, working on building the UI with Bootstrap

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { format } from 'date-fns';
+import _ from 'lodash';
+import bootstrap from 'bootstrap';
+
+// const date = new Date();
+// const dateFormat = format(date, 'MM/dd/yyyy');
+
+// function component() {
+//     const element = document.createElement('div');
+//     element.innerHTML = dateFormat;
+//     return element;
+//   }
+  
+// document.body.appendChild(component());
+
+class toDo {
+    constructor(_title, _desc, _due, _prio) {
+        this.title = _title;
+        this.description = _desc;
+        this.dueDate = _due;
+        this.priority = _prio;
+    }
+}
+
+let ex = new toDo('Shopping', 'Go shopping', 'tomorrow', 'high');
+console.log(ex);
